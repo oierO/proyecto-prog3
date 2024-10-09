c777916297d54ae40afbc9e722641633c75d99bc
Oier Ordorika <oier.ordorika@opendeusto.es>
2024-10-09 22:59:40 +0200

    Añadidas nuevas clases (Usuario + Vehiculo)

---
+++ proyecto-prog3/src/main/Usuario.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.util.*;
+
+public class Usuario {
+
+	protected String username;
+	protected String nombre;
+	protected String apellido;
+	protected ArrayList<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		while (!username.equals(null)) {
+			this.username = username;
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		while (!nombre.equals(null)) {
+			this.nombre = nombre;
+		}
+
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		while (!apellido.equals(null)) {
+			this.apellido = apellido;
+		}
+	}
+
+	public Usuario(String username, String nombre, String apellido) {
+		this.setUsername(username);
+		this.setNombre(nombre);
+		this.setApellido(apellido);
+	}
+
+	public ArrayList<Vehiculo> getVehiculos() {
+		return vehiculos;
+	}
+}
---
+++ proyecto-prog3/src/main/Vehiculo.java
@@ -0,0 +1,70 @@
+package main;
+
+import java.time.*;
+
+public class Vehiculo {
+	protected String matricula;
+	protected String marca;
+
+	protected void setMatricula(String matricula) {
+		while (!matricula.equals(null)) {
+			this.matricula = matricula;
+		}
+
+	}
+
+	protected void setMarca(String marca) {
+		while (!marca.equals(null)) {
+			this.marca = marca;
+		}
+
+	}
+
+	protected void setModelo(String modelo) {
+		while (!modelo.equals(null)) {
+			this.modelo = modelo;
+		}
+
+	}
+
+	protected void setAnoModelo(LocalDate anoModelo) {
+		this.anoModelo = anoModelo;
+	}
+
+	protected void setfMatricula(LocalDate fMatricula) {
+		this.fMatricula = fMatricula;
+	}
+
+	protected String modelo;
+	protected LocalDate anoModelo;
+	protected LocalDate fMatricula;
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public LocalDate getAnoModelo() {
+		return anoModelo;
+	}
+
+	public LocalDate getfMatricula() {
+		return fMatricula;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public Vehiculo(String matricula, String marca, String modelo, LocalDate anoModelo, LocalDate fMatricula) {
+		this.setMatricula(matricula);
+		this.setMarca(marca);
+		this.setModelo(modelo);
+		this.setAnoModelo(anoModelo);
+		this.setfMatricula(fMatricula);
+	}
+
+}
