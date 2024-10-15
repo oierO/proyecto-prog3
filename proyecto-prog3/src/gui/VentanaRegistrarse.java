9c31f685bdf5015b5cee61f826845cc52361139f
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2024-10-15 17:07:39 +0200

    Commit/VentanaRegistrarse

---
+++ proyecto-prog3/src/gui/VentanaRegistrarse.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class VentanaRegistrarse extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public VentanaRegistrarse() {
+		
+		
+	}
+	
+	
+
+}
