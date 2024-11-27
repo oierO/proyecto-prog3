ea46c652639fb8b0ab7807183cd0ae43fbc85153
Oier Ordorika <oier.ordorika@opendeusto.es>
2024-11-27 22:08:01 +0100

    Modificado formatter del parking y componentes.

--- proyecto-prog3/src/gui/ReservaParking.java
+++ proyecto-prog3/src/gui/ReservaParking.java
@@ -4,10 +4,15 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.GregorianCalendar;
 
 import javax.swing.*;
+import javax.swing.JFormattedTextField.AbstractFormatter;
+import javax.swing.JFormattedTextField.AbstractFormatterFactory;
+import javax.swing.text.DefaultFormatterFactory;
 
 import org.jdatepicker.JDatePicker;
 
@@ -21,7 +26,7 @@ public class ReservaParking extends JDialog {
 	 */
 	private static final long serialVersionUID = 1L;
 	private LocalDate fReserva;
-	
+
 	public ReservaParking(String planta, String plaza) {
 		System.out.println(plaza);
 		setModal(true);
@@ -48,14 +53,48 @@ public class ReservaParking extends JDialog {
 		selector.getModel().setDate(currentDate.getYear(), currentDate.getMonthValue() - 1,
 				currentDate.getDayOfMonth());
 		selector.getModel().setSelected(true);
+
+		selector.getFormattedTextField().setFormatterFactory(new AbstractFormatterFactory() {
+
+			@Override
+			public AbstractFormatter getFormatter(JFormattedTextField tf) {
+				AbstractFormatter pr = new AbstractFormatter() {
+
+					/**
+					 * 
+					 */
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public Object stringToValue(String text) throws ParseException {
+						String[] split = text.split("-");
+						GregorianCalendar cal = new GregorianCalendar(Integer.parseInt(split[0]),Integer.parseInt(split[1]),Integer.parseInt(split[2]));
+						return cal;
+					}
+
+					@Override
+					public String valueToString(Object value) throws ParseException {
+						GregorianCalendar cal = (GregorianCalendar) value;
+						String st = String.format("%s-%s-%s", cal.toZonedDateTime().getYear(),
+								cal.toZonedDateTime().getMonthValue(), cal.toZonedDateTime().getDayOfMonth());
+						return st;
+					}
+
+				};
+				return pr;
+			}
+
+		});
 		GregorianCalendar calendar = (GregorianCalendar) selector.getModel().getValue();
 		fReserva = calendar.toZonedDateTime().toLocalDate();
-		pFecha.add(selector);
-		selector.addActionListener(e -> System.out.println(selector.getFormattedTextField().getText()));
+
+		// selector.addActionListener(e ->
+		// System.out.println(selector.getFormattedTextField().getText()));
 		JPanel pTitulo = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JLabel titulo = new JLabel(String.format("Reservar plaza %s-%s...", plaza, planta));
 		Font fuente = new Font("Consolas", Font.BOLD, 15);
 		titulo.setFont(fuente);
+		pFecha.add(selector);
 		pTitulo.add(titulo);
 		add(pTitulo);
 		add(Box.createVerticalStrut(5));
