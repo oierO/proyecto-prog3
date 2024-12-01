d75c4b0164df4d1633ce3cdf67cc3d671874890d
IniMartinez <inigo.martinez.sebal@opendeusto.es>
2024-12-01 23:03:35 +0100

    Rendeder tabla vehiculos

--- proyecto-prog3/src/gui/PanelSesion.java
+++ proyecto-prog3/src/gui/PanelSesion.java
@@ -1,12 +1,16 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+
 import domain.Usuario;
 import domain.Vehiculo; // Importa la clase Vehiculo
 import main.DeustoTaller;
@@ -127,6 +131,45 @@ public class PanelSesion extends JPanel {
 	        panelVehiculo.add(scroll);
 	        
 	        panelCentral.add(panelVehiculo);
+	        
+	        tablaVehiculos.getTableHeader().setDefaultRenderer(new TableCellRenderer() {
+				
+				@Override
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+						int row, int column) {
+					JLabel l= new JLabel(value.toString());
+					l.setOpaque(true);
+					if (column == 0) {
+						l.setHorizontalAlignment(JLabel.CENTER);
+						l.setBackground(Color.RED);
+					} else if (column == 1) {
+						l.setHorizontalAlignment(JLabel.CENTER);
+						l.setBackground(Color.LIGHT_GRAY);
+					} else if (column == 2) {
+						l.setHorizontalAlignment(JLabel.CENTER);
+						l.setBackground(Color.GREEN);
+					} else if (column == 3) {
+						l.setHorizontalAlignment(JLabel.CENTER);
+						l.setBackground(Color.YELLOW);
+					
+				}
+					return l;
+				}
+			});
+	        tablaVehiculos.setDefaultRenderer(Object.class, new TableCellRenderer() {
+				
+				@Override
+				public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+						int row, int column) {
+					JLabel l= new JLabel(value.toString());
+					l.setOpaque(true);
+					if(row%2==0) {
+						l.setBackground(Color.CYAN);
+					}
+					
+					return l;
+				}
+			});
 		    
 		}
 
